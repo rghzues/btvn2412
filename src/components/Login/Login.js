68699ab621902aef4557c6cfa9999bba3262027f
rghzues <155547611+rghzues@users.noreply.github.com>
2025-12-24 22:15:49 +0700

    Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Paper,
+} from "@mui/material";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -17,31 +24,49 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    
+     <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper sx={{ p: 4, width: 350 }}>
+        <Typography variant="h5" align="center" mb={2}>
+          Login
+        </Typography>
 
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <Box component="form" onSubmit={handleLogin}>
+          <TextField
+            label="Email"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <br />
+          <TextField
+            label="Password"
+            type="password"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <br />
-
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          <Button
+            type="submit"
+            variant="contained"
+            fullWidth
+            sx={{ mt: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 
